abe0fe14a00a96d489363609944c98e507b448fd
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 17:15:38 -0500

    Qué nota necesito

--- Promgramas_Simples.py
+++ Promgramas_Simples.py
@@ -1,5 +1,9 @@
-decimal = float(input("Ingrese un número => "))
+nota_certamen1 = float(input("Ingrese nota certamen 1: "))
+nota_certamen2 = float(input("Ingrese nota certamen 2: "))
+nota_laboratorio = float(input("Ingrese nota laboratorio: "))
 
-parte_decimal = decimal - int(decimal)
+promedio_certamenes = (nota_certamen1 + nota_certamen2) / 2
 
-print(parte_decimal)
+nota_necesaria_certamen3 = (60 * 3 - promedio_certamenes * 0.7 - nota_laboratorio * 0.3)
+
+print("Necesita nota", nota_necesaria_certamen3, "en el certamen 3")
