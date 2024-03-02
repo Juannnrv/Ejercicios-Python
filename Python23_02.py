d11c2fb640220f058d6e8c1cc1f1e36ae9d142b3
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 16:25:56 -0500

    Factorial

--- Python23_02.py
+++ Python23_02.py
@@ -1,11 +1,5 @@
-numero1 = input("Dame un primer número => ")
-numero2 = input("Dame un segundo número => ")
-numero3 = input("Dame un tercer número => ")
-if numero1 > numero2 and numero1 > numero3:
-    print("el numero 1 es el mayor")
-elif numero2 > numero1 and numero2 > numero3:
-    print("el numero 2 es el mayor")
-elif numero3 > numero1 and numero3 > numero2:
-    print("el numero 3 es el mayor")
-else:
-    print("no te entiendo")
+num = int(input("Dame un número => "))
+fact=1
+for i in range(1, num+1):
+    fact*=i
+print("El factorial de",num, "es => ", fact)
