fef0a0727af801539495ce539c4eba9965681af5
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 16:30:58 -0500

    Lista de Números

--- Python23_02.py
+++ Python23_02.py
@@ -1,4 +1,9 @@
-num =int(input("¿Qué tabla de multiplicar quieres generar? =>"))
-for t in range(1, 11):
-    tabla = num * t
-    print(f"{num} * {t} = {tabla}")
+lista = int(input("Ingrese la cantidad de números que desea promediar => "))
+numeros = []
+for i in range(lista):
+    numero = float(input(f"Ingrese el número {i+1} => "))
+    numeros.append(numero)
+
+suma_total = sum(numeros)
+promedio = suma_total / lista
+print ("El promedio de los números ingresados es:", promedio)
