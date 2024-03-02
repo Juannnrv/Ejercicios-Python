2493d40bbc120d8faa2f205e4c0aa04092986098
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 16:23:00 -0500

    Número mayor

--- Python23_02.py
+++ Python23_02.py
@@ -1,4 +1,11 @@
-num1 = input("Dame un primer numero => ")
-num2 = input("Dame un segundo numero => ")
-suma = int(num1) + int(num2)
-print (suma)
+numero1 = input("Dame un primer número => ")
+numero2 = input("Dame un segundo número => ")
+numero3 = input("Dame un tercer número => ")
+if numero1 > numero2 and numero1 > numero3:
+    print("el numero 1 es el mayor")
+elif numero2 > numero1 and numero2 > numero3:
+    print("el numero 2 es el mayor")
+elif numero3 > numero1 and numero3 > numero2:
+    print("el numero 3 es el mayor")
+else:
+    print("no te entiendo")
