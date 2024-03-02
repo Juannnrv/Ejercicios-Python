d267e7e3f7437a46f03b39ad2237c4bcd7c1654e
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 16:28:06 -0500

    Número Primo

--- Python23_02.py
+++ Python23_02.py
@@ -1,5 +1,13 @@
+print("Vamos a ver cuales números son primos")
 num = int(input("Dame un número => "))
-fact=1
-for i in range(1, num+1):
-    fact*=i
-print("El factorial de",num, "es => ", fact)
+if num <= 1:
+    print("El número debe ser mayor que 1.")
+else:
+    es_primo = True
+    for i in range(2, num):
+        if num % i == 0:
+            es_primo = False
+if es_primo:
+    print(f"num es un número primo")
+else:
+    print(f"no es un número pirmo")
