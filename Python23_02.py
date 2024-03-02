b647d8388dee28b8b26bcaa3678c159d57710d0e
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 16:31:21 -0500

    Circulo

--- Python23_02.py
+++ Python23_02.py
@@ -1,9 +1,4 @@
-lista = int(input("Ingrese la cantidad de números que desea promediar => "))
-numeros = []
-for i in range(lista):
-    numero = float(input(f"Ingrese el número {i+1} => "))
-    numeros.append(numero)
-
-suma_total = sum(numeros)
-promedio = suma_total / lista
-print ("El promedio de los números ingresados es:", promedio)
+import math
+radio = float(input("Dame el radio de tu circulo =>"))
+area = math.pi * radio ** 2
+print("El área de tu circulo es =>", area)
