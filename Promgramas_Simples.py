ff6582e93eb29f13d6169812e38961a7a33b9592
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 17:16:59 -0500

    Huevos a la copa

--- Promgramas_Simples.py
+++ Promgramas_Simples.py
@@ -1,9 +1,22 @@
-nota_certamen1 = float(input("Ingrese nota certamen 1: "))
-nota_certamen2 = float(input("Ingrese nota certamen 2: "))
-nota_laboratorio = float(input("Ingrese nota laboratorio: "))
+import math
 
-promedio_certamenes = (nota_certamen1 + nota_certamen2) / 2
+M_pequeno = 47 
+M_grande = 67  
+rho = 1.038  
+c = 3.7  
+K = 5.4e-3  
+Tw = 100  
 
-nota_necesaria_certamen3 = (60 * 3 - promedio_certamenes * 0.7 - nota_laboratorio * 0.3)
+To = float(input("Ingrese la temperatura original del huevo (en °C): "))
 
-print("Necesita nota", nota_necesaria_certamen3, "en el certamen 3")
+tamanio_huevo = input("¿El huevo es pequeño o grande? (P/G): ")
+if tamanio_huevo.upper() == 'P':
+    M = M_pequeno
+else:
+    M = M_grande
+
+Ty = 70  
+t = (M**(2/3) * c * rho**(1/3) * K * math.pi**2) / (4 * math.pi / 3)**(2/3) * math.log(0.76 * (To - Tw) / (Ty - Tw))
+
+print("El tiempo necesario para alcanzar la temperatura máxima de la yema es de {:.2f} segundos.".format(t))
+
