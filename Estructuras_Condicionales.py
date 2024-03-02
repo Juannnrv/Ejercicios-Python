23f827751f7c6a06d460f13c64bb1008bbd6dad6
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 18:22:39 -0500

    Set tenis

--- Estructuras_Condicionales.py
+++ Estructuras_Condicionales.py
@@ -1,24 +1,33 @@
-from time import localtime
+juegos_ganados_A = int(input("Juegos ganados por A: "))
+juegos_ganados_B = int(input("Juegos ganados por B: "))
 
-print ("Ingrese su fecha de nacimiento")
-dia_nacimiento = int(input("Día => "))
-mes_nacimiento = int(input("Mes => "))
-anno_nacimiento = int(input("Año => "))
+# Es valido ?
+if juegos_ganados_A < 0 or juegos_ganados_B < 0 or abs(juegos_ganados_A - juegos_ganados_B) > 2:
+    print("Invalido")
+else:
+    # Ha terminado el set ?
+    if juegos_ganados_A < 6 and juegos_ganados_B < 6:
+        print("Aun no termina")
+    
+    elif juegos_ganados_A >= 6 and juegos_ganados_A - juegos_ganados_B >= 2:
+        print("Gano A")
+    elif juegos_ganados_B >= 6 and juegos_ganados_B - juegos_ganados_A >= 2:
+        print("Gano B")
+    
+    elif juegos_ganados_A == 6 and juegos_ganados_B == 6:
+        print("Aun no termina")
+    
+    elif juegos_ganados_A == 5 and juegos_ganados_B == 5:
+        print("Aun no termina")
+        
+    # Se define en un ultimo juego ?
+    elif juegos_ganados_A == 7 and juegos_ganados_B == 6:
+        print("Gano A")
+    elif juegos_ganados_B == 7 and juegos_ganados_A == 6:
+        print("Gano B")
+    else:
+        print("Invalido")
 
-# Obtener la fecha actual
-fecha_actual = localtime()
-
-# Obtener el día, mes y año actuales
-dia_actual = fecha_actual.tm_mday
-mes_actual = fecha_actual.tm_mon
-anno_actual = fecha_actual.tm_year
-
-edad = anno_actual - anno_nacimiento
-
-if mes_actual < mes_nacimiento or (mes_actual == mes_nacimiento and dia_actual < dia_nacimiento):
-    edad -= 1
-
-print("Usted tiene", edad, "años.")
 
 
 
