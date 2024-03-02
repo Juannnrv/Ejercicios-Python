a0ae8df9da9009c0d41082457ea4489507b74ebe
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 18:34:20 -0500

    Triángulos

--- Estructuras_Condicionales.py
+++ Estructuras_Condicionales.py
@@ -1,32 +1,17 @@
-juegos_ganados_A = int(input("Juegos ganados por A: "))
-juegos_ganados_B = int(input("Juegos ganados por B: "))
+a = float(input("Ingrese el lado a: "))
+b = float(input("Ingrese el lado b: "))
+c = float(input("Ingrese el lado c: "))
 
-# Es valido ?
-if juegos_ganados_A < 0 or juegos_ganados_B < 0 or abs(juegos_ganados_A - juegos_ganados_B) > 2:
-    print("Invalido")
+if a >= b + c or b >= a + c or c >= a + b:
+    print("No es un triángulo válido.")
 else:
-    # Ha terminado el set ?
-    if juegos_ganados_A < 6 and juegos_ganados_B < 6:
-        print("Aun no termina")
-    
-    elif juegos_ganados_A >= 6 and juegos_ganados_A - juegos_ganados_B >= 2:
-        print("Gano A")
-    elif juegos_ganados_B >= 6 and juegos_ganados_B - juegos_ganados_A >= 2:
-        print("Gano B")
-    
-    elif juegos_ganados_A == 6 and juegos_ganados_B == 6:
-        print("Aun no termina")
-    
-    elif juegos_ganados_A == 5 and juegos_ganados_B == 5:
-        print("Aun no termina")
-        
-    # Se define en un ultimo juego ?
-    elif juegos_ganados_A == 7 and juegos_ganados_B == 6:
-        print("Gano A")
-    elif juegos_ganados_B == 7 and juegos_ganados_A == 6:
-        print("Gano B")
+    # Que triangulo es ?
+    if a == b == c:
+        print("El triángulo es equilátero.")
+    elif a == b or a == c or b == c:
+        print("El triángulo es isósceles.")
     else:
-        print("Invalido")
+        print("El triángulo es escaleno.")
 
 
 
