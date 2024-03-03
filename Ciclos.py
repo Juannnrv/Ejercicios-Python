18fe3fa64745bb82826c64e11be795ccc06b9f8e
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-02 22:46:16 -0500

    Sumar entre números

--- Ciclos.py
+++ Ciclos.py
@@ -1,8 +1,9 @@
-num = int(input("Ingrese un número => "))
+num1 = int(input("Ingrese un número 1 => "))
+num2 = int(input("Ingrese un número 2 => "))
 
-for i in range(num + 1):
+suma_total = 0 # poder iniciar desde 0 
 
- resultado = 2 ** i
+for i in range(num1 + 1, num2):
+    suma_total += i # es como tener "suma_total = suma_total + i"
 
- print(f"{resultado}")
-
+print(f"La suma es {suma_total}")
